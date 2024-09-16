255ec62292e4af6a841013ffb02408ff6d41fa97
Meedux <herrerojayemark@gmail.com>
2024-09-16 16:10:01 +0800

    Navbar updates

--- components/Layout/NavbarContent.tsx
+++ components/Layout/NavbarContent.tsx
@@ -5,12 +5,12 @@ const NavbarContent = () => {
   return (
     <>
       <div className="flex-1">
-        <Link href="/" className="btn btn-ghost text-black text-xl">
+        <Link href="/" className="btn btn-ghost text-black text-4xl">
           DeepAscension
         </Link>
       </div>
       <div className="flex-none">
-        <button className="btn btn-square btn-ghost text-black">
+        {/* <button className="btn btn-square btn-ghost text-black">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -24,7 +24,15 @@ const NavbarContent = () => {
               d="M4 6h16M4 12h16M4 18h16"
             ></path>
           </svg>
-        </button>
+        </button> */}
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link className="text-black text-xl" href="/about">About</Link>
+          </li>
+          <li>
+            <Link className="text-black text-xl" href="#">Search Catalog</Link>
+          </li>
+        </ul>
       </div>
     </>
   );
