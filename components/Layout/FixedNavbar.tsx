4029a958e416e675b2758d8afe5f05191fb3f2a3
Meedux <herrerojayemark@gmail.com>
2024-09-16 16:11:36 +0800

    Build fixes

--- components/Layout/FixedNavbar.tsx
+++ components/Layout/FixedNavbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Link from "next/link"; // Assuming you're using Next.js
 import NavbarContent from "./NavbarContent";
 
 const FixedNavbar = () => {
